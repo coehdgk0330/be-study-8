8f65a99cfce0927cff6c8e0047eca7895c757117
coehdgk0330 <coehdgk0330@gmail.com>
2026-05-06 13:57:59 +0900

    자바 시작, .gitignore 추가

---
+++ src/be_study/start/HellowWorld.java
@@ -0,0 +1,22 @@
+package be_study.start;
+
+public class HellowWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello, World!!");
+		
+		// 자바 코드 실행 : Ctrl + F11
+		
+		// 주속 메모역할
+		
+		// 한줄 주석
+			
+		/* 여러줄 주석
+		 ㅇㄹㅇㄹㅇㄹㅇ*/
+		 
+		System.out.println("한글");
+	}
+
+}
