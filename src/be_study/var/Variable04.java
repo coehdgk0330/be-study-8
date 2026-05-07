c852d793ee873ac5abdfd81473c22a6f4e4a6d25
coehdgk0330 <coehdgk0330@gmail.com>
2026-05-07 12:37:30 +0900

    변수 타입변환

---
+++ src/be_study/var/Variable04.java
@@ -0,0 +1,44 @@
+package be_study.var;
+
+public class Variable04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			String s1 = "12";
+			int n1 =11;
+			int n2= 22;
+			
+			System.out.println(n1 + n2+ s1); // 11 + 22 + "12"
+			System.out.println(n1 + s1 +n2); // 11 + "12" + 22
+			
+			String s2 = "1234";
+			int n3 = 16;
+			System.out.println(s2 + n3);  //123416
+			
+			//String 문자열 -> 숫자 (정수/실수) (int/double)
+			
+			System.out.println( Integer.parseInt(s2) + n3);
+							//	Integer.parseInt("1234")
+			
+			
+			String s3 = "33.55";
+			//int n4 = Integer.parseInt(s3);
+			// 실수형태의 문자열 -> int 변환 시도 XXX
+			
+			double d4 = Double.parseDouble(s3); // "33.55" -> 33.55
+			System.out.println( d4 + 12.1);
+			
+			String s4 = "12a.12b";
+			//Double.parseDouble(s4); // 숫자형태가 아닌 문자열을 숫자형으로 변환 시도
+			
+			boolean b1 =true ; //false; 참/거짓
+			
+			String s5 = "false"; // 문자열 "false" 
+			
+			boolean b2 = Boolean.parseBoolean(s5);
+			System.out.println(b2);
+			System.out.println(s5);
+	}
+
+}
