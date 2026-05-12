d6129ab6eb2ce56f0543c5d2d8e1efd2cfb6786a
coehdgk0330 <coehdgk0330@gmail.com>
2026-05-12 16:26:33 +0900

    퀴즈 11 반복문

---
+++ src/be_study/quiz/Quiz11.java
@@ -0,0 +1,18 @@
+package be_study.quiz;
+
+public class Quiz11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0; i<5; i++){	
+			
+			System.out.println(12345);
+		}
+		for (int i = 1; i <= 5; i++) { 
+		    for (int j = 1; j <= 5; j++) { 
+		        System.out.print(i); 
+		    }
+		    System.out.println(); 
+		}
+	}
+}
