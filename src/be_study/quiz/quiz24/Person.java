254d20e54c00c5ed250d5a532e25653839909af8
coehdgk0330 <coehdgk0330@gmail.com>
2026-05-20 15:00:56 +0900

    퀴즈 24 클래스

---
+++ src/be_study/quiz/quiz24/Person.java
@@ -0,0 +1,26 @@
+package be_study.quiz.quiz24;
+
+public class Person {
+		
+	
+		//필드
+	
+		String name;
+		String gender;
+		int age;
+		int height;
+		int weight;
+		String job;
+		String blood;
+		String health;
+		String sleep;
+		//생성자
+		
+		Person(){};
+		
+		
+		
+		
+		
+		
+}
---
+++ src/be_study/quiz/quiz24/PersonMain.java
@@ -0,0 +1,38 @@
+package be_study.quiz.quiz24;
+
+public class PersonMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Person Person1 = new Person();
+			
+			Person1.name="홍길동";
+			Person1.gender="남자";
+			Person1.age=20;
+			Person1.height=180;
+			Person1.weight=70;
+			Person1.job="학생";
+			Person1.blood="AB";
+			Person1.health="Good";
+			Person1.sleep="X";
+			
+			System.out.println(Person1.name);
+			System.out.println(Person1.gender);
+			System.out.println(Person1.age);		
+			System.out.println(Person1.height);
+			System.out.println(Person1.weight);
+			System.out.println(Person1.job);
+			System.out.println(Person1.blood);
+			System.out.println(Person1.health);
+			System.out.println(Person1.sleep);
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+
+}
