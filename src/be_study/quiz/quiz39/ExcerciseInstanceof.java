9b121fc28605b705d27d8d2e8a16512175a60f96
coehdgk0330 <coehdgk0330@gmail.com>
2026-06-02 11:50:39 +0900

    퀴즈 39

---
+++ src/be_study/quiz/quiz39/ExcerciseInstanceof.java
@@ -0,0 +1,43 @@
+package be_study.quiz.quiz39;
+
+	//basecode
+	class ExcerciseInstanceof {
+	public static void main(String[] args) {
+	Robot[] arr = { new DanceRobot(), new SingRobot(), new DrawRobot()};
+	for(int i=0; i< arr.length;i++)
+	action(arr[i]);
+	// main
+	}
+	
+	
+	public static void action(Robot r) {
+	if (r instanceof DanceRobot d) {
+		d.dance();
+	}else if ( r instanceof SingRobot s) {
+		s.sing();
+	}else if ( r instanceof DrawRobot dr) {
+		dr.draw();
+	}
+	}
+	}
+	
+	class Robot {}
+	
+	class DanceRobot extends Robot {
+	void dance() {
+	System.out.println("춤을 춥니다.");
+	}
+	}
+	
+	class SingRobot extends Robot {
+	void sing() {
+	System.out.println("노래를 합니다.");
+	}
+	}
+	
+	class DrawRobot extends Robot {
+	void draw() {
+	System.out.println("그림을 그립니다.");
+	}
+	}
+	
