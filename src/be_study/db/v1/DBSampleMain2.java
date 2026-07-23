07140b3eb2e8f0c84608fbfa1e32fad2afe94beb
coehdgk0330 <coehdgk0330@gmail.com>
2026-07-23 11:44:45 +0900

    DB 연동 select 조회 활용 -2.1

---
+++ src/be_study/db/v1/DBSampleMain2.java
@@ -0,0 +1,213 @@
+package be_study.db.v1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBSampleMain2 {
+
+	public static void main(String[] args) {
+		
+		List<Dept> deptList1 = findDeptList();
+		
+		if(deptList1 == null) {
+			System.out.println("조회 데이터가 없다");
+		}else {
+			for(Dept d : deptList1) {
+				System.out.println(d.toString());
+			}
+		}
+		
+		List<Dept> deptList2 = findDeptList2();
+		
+		if(deptList2.size() == 0) {
+			System.out.println("데이터가없다");
+		}else {
+			for(Dept d : deptList2) {
+				System.out.println(d.toString());
+			}
+		}
+		}
+	
+	
+	public static List<Dept> findDeptList() {
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		//DB 연결 정보
+		
+		String db_url = "jdbc:oracle:thin:@localhost:1521:orcl";
+		String db_id = "scott";
+		String db_pw = "tiger";
+		
+		
+		//db 연결, 실행 객체
+		Connection conn = null; //db연결
+		PreparedStatement psmt = null; //db 연결해서 sql 명령 실행해주는 객체
+		ResultSet rs = null; //sql 실행 후 select 결과를 저장하는 객체
+		
+		
+		//DB 연결
+		
+		
+		try {
+			conn = DriverManager.getConnection(db_url, db_id, db_pw);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		//실행할 쿼리 준비
+		String sqlQuery = " select * from dept ";
+		
+		List<Dept> deptList = null;
+		
+		//쿼리 실행, 실행 후 후속 데이터 처리
+		
+		try {
+			psmt = conn.prepareStatement(sqlQuery);
+			rs = psmt.executeQuery();
+			
+			while(rs.next()) { //다음에 읽어올 데이터(행단위)가 있는가? true 다음 데이터가 있다
+				// rs가 하나의 행을 가리키고, 열단위 값을 기준으로 조회
+				
+				// 데이터가 있다.
+				
+				// 한줄 조회 한 행 데이터
+				// 한 행 데이터 -> Dept 객체 저장
+				// List<Dept>  .add(Dept)
+				
+				// column 이름(별칭) 기준 조회
+				Dept dept = new Dept();
+				
+				dept.setDeptno(rs.getInt("deptno") );
+				dept.setDname(rs.getString("dname") );
+				dept.setLoc(rs.getString("loc") );
+				
+				if(deptList == null) {
+					deptList = new ArrayList<>();
+				}
+				deptList.add(dept);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+			
+		//DB 연결 종료
+			
+		try {
+			
+			if(rs !=null)
+			rs.close();		//null 객체 메소드 호출 -> NUL
+			
+			if(psmt!=null)
+			psmt.close();
+			
+			if(conn !=null)
+			conn.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return deptList;
+			
+}	
+	
+	public static List<Dept> findDeptList2() {
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		//DB 연결 정보
+		
+		String db_url = "jdbc:oracle:thin:@localhost:1521:orcl";
+		String db_id = "scott";
+		String db_pw = "tiger";
+		
+		
+		//db 연결, 실행 객체
+		Connection conn = null; //db연결
+		PreparedStatement psmt = null; //db 연결해서 sql 명령 실행해주는 객체
+		ResultSet rs = null; //sql 실행 후 select 결과를 저장하는 객체
+		
+		
+		//DB 연결
+		
+		
+		try {
+			conn = DriverManager.getConnection(db_url, db_id, db_pw);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		//실행할 쿼리 준비
+		String sqlQuery = " select * from dept ";
+		
+		List<Dept> deptList = null;
+		
+		//쿼리 실행, 실행 후 후속 데이터 처리
+		
+		try {
+			psmt = conn.prepareStatement(sqlQuery);
+			rs = psmt.executeQuery();
+			
+			while(rs.next()) { //다음에 읽어올 데이터(행단위)가 있는가? true 다음 데이터가 있다
+				// rs가 하나의 행을 가리키고, 열단위 값을 기준으로 조회
+				
+				// 데이터가 있다.
+				
+				// 한줄 조회 한 행 데이터
+				// 한 행 데이터 -> Dept 객체 저장
+				// List<Dept>  .add(Dept)
+				
+				// column 이름(별칭) 기준 조회
+				Dept dept = new Dept();
+				
+				dept.setDeptno(rs.getInt("deptno") );
+				dept.setDname(rs.getString("dname") );
+				dept.setLoc(rs.getString("loc") );
+				
+				if(deptList == null) {
+					deptList = new ArrayList<>();
+				}
+				deptList.add(dept);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+			
+		//DB 연결 종료
+			
+		try {
+			
+			if(rs !=null)
+			rs.close();		//null 객체 메소드 호출 -> NUL
+			
+			if(psmt!=null)
+			psmt.close();
+			
+			if(conn !=null)
+			conn.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return deptList;
+			
+}
+
+}
