bf2ed5a90d7c12764876cab01eb7ab85b767a6ba
coehdgk0330 <coehdgk0330@gmail.com>
2026-07-29 10:30:01 +0900

    공공데이터 API DB에 데이터 저장 (DAO 추가)

--- src/be_study/api/v2/MidFcstInfoServiceRepository.java
+++ src/be_study/api/v2/MidFcstInfoServiceRepository.java
@@ -5,6 +5,11 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import java.io.IOException;
 
 public class MidFcstInfoServiceRepository {
@@ -13,15 +18,15 @@ public class MidFcstInfoServiceRepository {
 	 	
 	 */
 	
-	public String requestApi_getMidTa() throws Exception{
+	public String requestApi_getMidTa(String tmFc, String regId) throws Exception{
 		
 		StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/MidFcstInfoService/getMidTa"); /*URL*/
         urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=1a3a5b7d38addaa0e8fb99abf2a2faa1d92faa2992b6b04e5465560f0951260d"); /*Service Key*/
         urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
         urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
         urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*요청자료형식(XML/JSON)Default: XML*/
-        urlBuilder.append("&" + URLEncoder.encode("regId","UTF-8") + "=" + URLEncoder.encode("11B10101", "UTF-8")); /*11B10101 서울, 11B20201 인천 등 ( 별첨엑셀자료 참고)*/
-        urlBuilder.append("&" + URLEncoder.encode("tmFc","UTF-8") + "=" + URLEncoder.encode("202607280600", "UTF-8")); /*-일 2회(06:00,18:00)회 생성 되며 발표시각을 입력- YYYYMMDD0600(1800) 최근 24시간 자료만 제공*/
+        urlBuilder.append("&" + URLEncoder.encode("regId","UTF-8") + "=" + URLEncoder.encode("regId", "UTF-8")); /*11B10101 서울, 11B20201 인천 등 ( 별첨엑셀자료 참고)*/
+        urlBuilder.append("&" + URLEncoder.encode("tmFc","UTF-8") + "=" + URLEncoder.encode("tmFc", "UTF-8")); /*-일 2회(06:00,18:00)회 생성 되며 발표시각을 입력- YYYYMMDD0600(1800) 최근 24시간 자료만 제공*/
         URL url = new URL(urlBuilder.toString());
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
@@ -45,6 +50,95 @@ public class MidFcstInfoServiceRepository {
         return sb.toString();
     }
 
+		public MidTaDTO getMidTa(String tmFc, String regId) {
+			
+			MidTaDTO midTaDTO = null;
+			
+			try {
+				String jsonString = requestApi_getMidTa(tmFc, regId);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			try {
+				//API 요청
+				//String jsonString = repository.requestApi_getMidTa();
+				
+				//응답 텍스트
+				
+				String jsonString = "{\"response\":{\"header\":{\"resultCode\":\"00\",\"resultMsg\":\"NORMAL_SERVICE\"},\"body\":{\"dataType\":\"JSON\",\"items\":{\"item\":[{\"regId\":\"11B10101\",\"taMin4\":26,\"taMin4Low\":1,\"taMin4High\":1,\"taMax4\":33,\"taMax4Low\":1,\"taMax4High\":1,\"taMin5\":25,\"taMin5Low\":1,\"taMin5High\":1,\"taMax5\":34,\"taMax5Low\":1,\"taMax5High\":1,\"taMin6\":25,\"taMin6Low\":1,\"taMin6High\":1,\"taMax6\":35,\"taMax6Low\":1,\"taMax6High\":1,\"taMin7\":26,\"taMin7Low\":1,\"taMin7High\":1,\"taMax7\":35,\"taMax7Low\":1,\"taMax7High\":1,\"taMin8\":26,\"taMin8Low\":0,\"taMin8High\":1,\"taMax8\":35,\"taMax8Low\":0,\"taMax8High\":1,\"taMin9\":25,\"taMin9Low\":0,\"taMin9High\":1,\"taMax9\":35,\"taMax9Low\":0,\"taMax9High\":1,\"taMin10\":25,\"taMin10Low\":0,\"taMin10High\":1,\"taMax10\":35,\"taMax10Low\":0,\"taMax10High\":1}]},\"pageNo\":1,\"numOfRows\":10,\"totalCount\":1}}}";
+				
+				System.out.println("---------main 에서 json 응답 출력 ---------");
+				System.out.println(jsonString);
+				
+				//단순 JSON 포맷의 텍스트 -> 자바에서 활용하는 기준으로 파싱
+
+				//json-simple 라이브러리 활용
+				
+				// 최초 : 파싱객체 JSONParser()   parse() 
+				// {	JSONObject
+				// [	JSONArray
+				// key:value	get("키값") 
+				
+				
+				JSONParser jsonParser = new JSONParser();
+				JSONObject jsonObj = (JSONObject)jsonParser.parse(jsonString);
+				
+				JSONObject response = (JSONObject)jsonObj.get("response");
+				
+				JSONObject header = (JSONObject)response.get("header");
+				
+				System.out.println(header.get("resultCode"));
+				System.out.println(header.get("resultMsg"));
+				
+				JSONObject body = (JSONObject)response.get("body");
+				
+				System.out.println(body.get("dataType"));
+				System.out.println(body.get("pageNo"));
+				System.out.println(body.get("numOfRows"));
+				System.out.println(body.get("totalCount"));
+				
+				JSONObject items = (JSONObject)body.get("items");
+				JSONArray item = (JSONArray)items.get("item");
+				
+//				for(int i=0; i<item.size(); i++) {
+//					item.get(i);
+//				}
+				
+				JSONObject item0 = (JSONObject)item.get(0);
+				
+				System.out.println(item0.get("regId"));
+				System.out.println(item0.get("taMin4"));
+				System.out.println(item0.get("taMax4"));
+				System.out.println(item0.get("taMin5"));
+				System.out.println(item0.get("taMax5"));
+				
+				
+				midTaDTO =new MidTaDTO();
+				
+				midTaDTO.setResultCode(header.get("resultCode").toString());
+				midTaDTO.setResultMsg(header.get("resultMsg").toString());
+				
+				
+				midTaDTO.setTmFc(tmFc);
+				midTaDTO.setRegId(item0.get("regId").toString());
+				midTaDTO.setTaMin4(((Number)(item0.get("taMin4"))).intValue() );
+				midTaDTO.setTaMax4(Integer.parseInt(item0.get("taMax4").toString() ) );
+				midTaDTO.setTaMin5(((Number)(item0.get("taMin5"))).intValue() );
+				midTaDTO.setTaMax5(((Number)(item0.get("taMax5"))).intValue() );
+				
+//				System.out.println(midTaDTO);
+				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		
+			
+			return midTaDTO;
+		}
 
 	
 }
